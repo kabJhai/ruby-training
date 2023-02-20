65fc51e1af2abc12f271399026b1746e0610ba2e
Kabila Haile <kabilahaile@gmail.com>
2023-02-20 17:54:57 -0600

    Strings

---
+++ strings.rb
@@ -0,0 +1,20 @@
+#String
+
+#Stings can be declared by using single quotes or double quotes.
+
+#Most operations are the same as python's built-in string functions.
+
+#Appending a string to a string: is a little bit different.
+
+another = "Something"
+
+another << "Something more"
+puts another
+
+#Escape characters is the same as python's built-in string functions.
+
+#String interpolation
+
+message = "This is a message"
+
+puts "The message in this code is #{message}"
