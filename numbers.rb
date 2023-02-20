e2636dd5240216e9d8b3dc09665a64d606618285
Kabila Haile <kabilahaile@gmail.com>
2023-02-20 17:35:56 -0600

    integers

---
+++ numbers.rb
@@ -0,0 +1,17 @@
+#Numbers
+
+# Ruby has integers and floats
+
+# Ruby follows the PEMDAS rule for integers and floats
+
+# Contains the same behavior as Python for Mathematical operations
+
+# Ruby has multiple methods on objets
+
+#To get the class of an object object you have to use the methods class
+
+puts 100.class
+
+#The method above prints the class on the object
+
+puts 100.next
