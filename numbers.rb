a85f342e633cc486a1a3f94fcd8c2834f3ecac78
Kabila Haile <kabilahaile@gmail.com>
2023-02-20 17:43:10 -0600

    Numbers

--- numbers.rb
+++ numbers.rb
@@ -11,7 +11,39 @@
 #To get the class of an object object you have to use the methods class
 
 puts 100.class
+puts 100.0.class
 
 #The method above prints the class on the object
 
 puts 100.next
+
+#Float follows the same rule as in Python
+
+#Except
+
+#To convert to an int
+
+puts 100.0.to_i
+
+#To convert to a float
+
+puts 100.to_f
+
+#To convert to a string
+
+puts 100.to_s
+
+#If either the operand or the operator;  or the numerator or the denominator are float the result will be a float
+
+puts 10 / 4
+
+puts 10.0 / 4
+
+# Other methods like ceil, floor, round, etc can also be called
+
+puts 12.4.ceil
+
+puts 112.4.floor
+
+puts 12.4.round
+
