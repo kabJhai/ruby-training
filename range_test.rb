fc1ca66f26be3f0c58f31c78ef68d13f6b4ce856
Kabila Haile <kabilahaile@gmail.com>
2023-02-21 09:48:45 -0600

    Range

---
+++ range_test.rb
@@ -0,0 +1,18 @@
+#Experimenting with ranges
+
+# If you want to declare ranges you have to a range in between values that can be represented as ranges like numbers and strings
+
+# There are two types of ranges: 
+#     Inclusive Range (It is declared with two '.'s)
+#     Exclusive Range (It is declared with three '.'s)
+
+inclusive_example = 1..100
+
+exclusive_example = 1...100
+
+# The exclusive range doesn't include the last element of the range
+
+# You can find the first and the last elements with methods like begin,first,last and end.
+
+# Mainly used with arrays and iterators.
+
